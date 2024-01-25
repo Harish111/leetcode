72fe848337778d9fe635d4a2ba692a95acd39e6a
harishgurram <gurramharish275@gmail.com>
2024-01-25 14:21:47 +0530

    the solution is paassing 16/47 test cases

---
+++ LongestCommonPrefix.py
@@ -0,0 +1,51 @@
+"""1143. Longest Common Subsequence
+
+Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+For example, "ace" is a subsequence of "abcde".
+A common subsequence of two strings is a subsequence that is common to both strings.
+
+ 
+
+Example 1:
+
+Input: text1 = "abcde", text2 = "ace" 
+Output: 3  
+Explanation: The longest common subsequence is "ace" and its length is 3.
+Example 2:
+
+Input: text1 = "abc", text2 = "abc"
+Output: 3
+Explanation: The longest common subsequence is "abc" and its length is 3.
+Example 3:
+
+Input: text1 = "abc", text2 = "def"
+Output: 0
+Explanation: There is no such common subsequence, so the result is 0.
+"""
+
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        """
+        :type text1: str
+        :type text2: str
+        :rtype: int
+        """
+        op = 0
+        last_index =0
+        for index in range(len(text1)):
+            char = text1[index]
+            print("char="+char+",diff="+str(last_index)+", curr="+str(index))
+            if (char in text2) and (index - last_index)<=2:
+                last_index = index
+                print("yes")
+                text2.replace(char,'',1)
+                op+=1
+                
+                
+        
+        
+        return op
+        
