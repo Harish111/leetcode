5231117320f73bcb05617b3e16753d4f364f7d0d
harishgurram <gurramharish275@gmail.com>
2024-01-26 13:30:30 +0530

    SetMismatch leetcode

--- SetMismatch.py
+++ SetMismatch.py
@@ -3,4 +3,16 @@ You have a set of integers s, which originally contains all the numbers from 1 t
 
 You are given an integer array nums representing the data status of this set after the error.
 
-Find the number that occurs twice and the number that is missing and return them in the form of an array."""
+Find the number that occurs twice and the number that is missing and return them in the form of an array.
+
+Example 1:
+
+Input: nums = [1,2,2,4]
+Output: [2,3]
+Example 2:
+
+Input: nums = [1,1]
+Output: [1,2]
+
+"""
+
